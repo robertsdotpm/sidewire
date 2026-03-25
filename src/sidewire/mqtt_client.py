18a89ba26bec478e9daac347804ea0f318ee0ed5
Matthew Roberts <matthew@roberts.pm>
2026-03-25 14:47:30 +1100

    Adding application level acks.

--- src/sidewire/mqtt_client.py
+++ src/sidewire/mqtt_client.py
@@ -25,7 +25,7 @@ send(msg, dest_pub_key, plugin_id)
 
 topic (33 their pub key)
     in bytes -- double all for hex
-    msg format: 33 our_pub_key, 64 sig over ( 32 plugin_id, msg )
+    msg format: 33 our_pub_key, 64 sig over ( 32 plugin_id, 4 seq_no, msg )
     ^ allows ack back
 
 do mqtt packet ides first then do the app level ack stuff
@@ -34,12 +34,17 @@ do mqtt packet ides first then do the app level ack stuff
 
 import hashlib
 import asyncio
+from enum import IntEnum
 from aionetiface import *
 from .utils import *
 from .signing import *
 from .mqtt_packet import *
 
 MQTT_KEEP_ALIVE = 60
+
+class MsgEnum(IntEnum):
+    MSG = 1
+    MSGACK = 2
 
 class ProtoEvent:
     def __init__(self, event_type, params):
@@ -93,7 +98,9 @@ class MQTTClient:
         pipe.add_msg_cb(self.mqtt_packet_reader)
 
         # Subscribe to messages to our own public key.
-        await self.subscribe(self.kp.compact_public_key)
+        pub_hex = to_hs(self.kp.compact_public_key)
+        assert(len(pub_hex) == 66)
+        await self.subscribe(pub_hex)
 
         # Periodically send ping requests to server.
         keep_alive = int(MQTT_KEEP_ALIVE / 2)
@@ -102,9 +109,7 @@ class MQTTClient:
         )
 
     async def subscribe(self, topic, timeout=4):
-        # Stick to ascii for topics.
-        if type(topic) == bytes:
-            topic = to_h(topic)
+        assert(type(topic) == str)
 
         # Already subscribed.
         if topic in self.subscriptions:
@@ -127,42 +132,58 @@ class MQTTClient:
         print("acked subscribe.")
         self.subscriptions.add(topic)
 
-    async def send(self, msg, dest_pk_hex, plugin_id):
-        assert(len(plugin_id) == 64)
+    async def send(self, msg, dest_pk_hex, plugin_id_hex, msg_type=MsgEnum.MSG, seq_no=None):
+        assert(len(plugin_id_hex) == 64)
         assert(len(dest_pk_hex) == 66)
 
+        # Prepend application-level header to message portion.
+        msg = to_h(bytes([msg_type])) + msg
+
+        # Create queue per plugin ID.
+        if msg_type == MsgEnum.MSG:
+            if plugin_id_hex not in self.msg_queues:
+                self.msg_queues[plugin_id_hex] = []
+
+        # Get seq no
+        print("seq no =", seq_no)
+        if seq_no is None:
+            seq_no = len(self.msg_queues[plugin_id_hex])
+        seq_no_hex = "{:08x}".format(seq_no)
+
         # Signed message section.
-        signed_msg = plugin_id + msg
+        signed_msg = plugin_id_hex + seq_no_hex + msg
         sig = self.kp.private_key.sign(
             to_b(signed_msg),
             sigencode=util.sigencode_string
         )
-        sig = to_h(sig)
-        assert(len(sig) == 128)
+        sig_hex = to_h(sig)
+        assert(len(sig_hex) == 128)
 
         # Our public key.
         src_pk_hex = to_h(self.kp.compact_public_key) # 66
         assert(len(src_pk_hex) == 66)
 
         # Full proto message to send.
-        out = src_pk_hex + sig + plugin_id + msg
+        out = src_pk_hex + sig_hex + signed_msg
         assert(type(out) == str)
-        assert(len(out) == (258 + len(msg)))
-        
-        # Create queue per plugin ID.
-        if plugin_id not in self.msg_queues:
-            self.msg_queues[plugin_id] = []
+        assert(len(out) == (266 + len(msg)))
 
         # Queue message.
-        self.msg_queues[plugin_id].append({
-            "attempts": 0,
-            "dest_pk_hex": dest_pk_hex,
-            "out": out,
-            "acked": asyncio.Event()
-        })
+        ack_future = asyncio.Future()
+        if msg_type == MsgEnum.MSG:
+            self.msg_queues[plugin_id_hex].append({
+                "attempts": 0,
+                "dest_pk_hex": dest_pk_hex,
+                "seq_no": seq_no,
+                "out": out,
+                "acked": ack_future
+            })
         
         # Publish the message as intended.
         await self.publish(dest_pk_hex, out)
+
+        # Caller can await ack if they want.
+        return ack_future
 
     def get_unique_packet_id(self, packet_type):
         for _ in range(0, 10000):
@@ -186,16 +207,16 @@ class MQTTClient:
         return packet_ack
 
     async def handle_mqtt_packet(self, packet):
-        packet.debug_print()
+        #packet.debug_print()
 
-        print("in handle mqtt pack")
+        #print("in handle mqtt pack")
 
         # Main packets to 
         for packet_type in (MQTTEnum.PUBACK, MQTTEnum.SUBACK):
             if packet_type == packet.type:
-                print("got mqtt ack")
-                print("puback var header = ", packet.body)
-                print("len body = ", len(packet.body))
+                #print("got mqtt ack")
+                #print("puback var header = ", packet.body)
+                #print("len body = ", len(packet.body))
 
                 #assert(len(packet.body) == 2)
                 # Strip packet_id from variable header.
@@ -210,16 +231,21 @@ class MQTTClient:
                 
                 # Sets the return code only for SUBACK otherwise empty str.
                 ack_future.set_result(packet.body[2:])
-                print("ack packet id", packet_id)
+                #print("ack packet id", packet_id)
 
         # Handle receive channel message.
         if MQTTEnum.PUBLISH == packet.type:
+            print("got mqtt publish")
             out = mqtt_parse_publish(packet)
+            print(out)
+
             if not out:
+                print("invalid publish packet")
                 return
             
             topic, payload, packet_id = out
             if topic not in self.subscriptions:
+                print("topic not in subscriptions.")
                 return
             
             assert(is_ascii(topic))
@@ -229,20 +255,66 @@ class MQTTClient:
             if compact_public_key != self.kp.compact_public_key:
                 print("Recv msg not meant for us.")
 
-            src_pk_hex = h_to_b(payload[:66]); p = 66
+            print("payload = ", payload)
+
+            src_pk_hex = payload[:66]; p = 66
             sig = h_to_b(payload[p:p + 128]); p += 128
             signed_msg = to_b(payload[p:])
-            plugin_id = payload[p:p + 64]; p += 64
+            plugin_id_hex = payload[p:p + 64]; p += 64
+            seq_no_hex = payload[p:p + 8]; p += 8
             msg = payload[p:]
 
-            print(
-                "siggn verify result =",
-                self.kp.public_key.verify(
-                    sig,
-                    signed_msg,
-                    sigdecode=util.sigdecode_string
-                )
+
+            vk = VerifyingKey.from_string(
+                h_to_b(src_pk_hex),
+                curve=SECP256k1
             )
+
+            is_valid_sig = vk.verify(
+                sig,
+                signed_msg,
+                sigdecode=util.sigdecode_string
+            )
+
+            if not is_valid_sig:
+                print("invalid sig for ", msg)
+                return
+            
+            msg_type = h_to_b(msg[:2])[0]
+            msg = msg[2:]
+            print("msg type = ", msg_type)
+
+            # If a regular message is received then send an ACK back to owner.
+            if MsgEnum.MSG:
+                print("sending back ack to src")
+                await self.send(
+                    "ack",
+                    src_pk_hex,
+                    plugin_id_hex,
+                    MsgEnum.MSGACK,
+                    seq_no=int(seq_no_hex, 16)
+                )
+                
+                return
+            
+            # Handle ACK response.
+            if MsgEnum.MSGACK == msg_type:
+                print("got msg ack")
+                if plugin_id_hex not in self.msg_queues:
+                    print("msg ack: in valid plugin id")
+                    return
+                
+                seq_no = int(seq_no_hex, 16)
+                if seq_no > len(self.msg_queues[plugin_id_hex]):
+                    print("msg ack seq no invalid")
+                    return
+                
+                msg_meta = self.msg_queues[plugin_id_hex][seq_no]
+                if msg_meta["acked"].done():
+                    return
+                
+                msg_meta["acked"].set_result(True)
+                return
 
             print("signed message for us = ", msg)
 
@@ -250,7 +322,7 @@ class MQTTClient:
 
         # Handle ping from server.
         if MQTTEnum.PINGRESP == packet.type:
-            print("got ping response.")
+            #print("got ping response.")
             return
     
     async def mqtt_keep_alive(self):
@@ -260,9 +332,9 @@ class MQTTClient:
 
     # TCP streaming protocol handler for MQTT.
     async def mqtt_packet_reader(self, chunk, client_tup, pipe):
-        print("got chunk = ", chunk)
+        #print("got chunk = ", chunk)
         if not chunk:
-            print("not chunk")
+            #print("not chunk")
             return
 
         # append incoming data
@@ -293,11 +365,13 @@ class MQTTClient:
             pkt_buf = self.buf[:total_len]
             self.buf = self.buf[total_len:]
 
-            print("pkt buf = ", pkt_buf)
+            #print("pkt buf = ", pkt_buf)
 
             # parse + handle
             pkt = mqtt_parse_packet(pkt_buf)
-            await self.handle_mqtt_packet(pkt)
+            await async_wrap_errors(
+                self.handle_mqtt_packet(pkt)
+            )
 
 async def load_signal_pipes(af, nic, seed_str, n, filter_list=[]):
     # Monitor incorrectly lists TCP servers under UDP.
@@ -331,18 +405,29 @@ async def workspace():
     nic = Interface("default")
 
 
+    # Connect Alice client.
     alice_kp = Signing.keypair()
     alice_plugin_id = hashlib.sha256(b"alice plugin").hexdigest()
     alice_client = MQTTClient(IP4, nic, ("ovh1.p2pd.net", 1883), alice_kp)
     await alice_client.connect()
-    await asyncio.sleep(2)
-    await alice_client.send(
-        "hello thar", 
-        to_h(alice_kp.compact_public_key),
+
+    # Connect Bob client.
+    bob_kp = Signing.keypair()
+    bob_plugin_id = hashlib.sha256(b"bob plugin").hexdigest()
+    bob_client = MQTTClient(IP4, nic, ("ovh1.p2pd.net", 1883), bob_kp)
+    await bob_client.connect()
+
+    # Send a message from alice to bob.
+    bob_ack_msg = await alice_client.send(
+        "hello bob -- with ordering and ack", 
+        # Destination channel is Bob's public key hex.
+        to_hs(bob_kp.compact_public_key),
         alice_plugin_id
     )
     
-
+    # Wait for alice to receive the message.
+    await bob_ack_msg
+    print("got ack from bob")
     await asyncio.sleep(4)
     return
 
